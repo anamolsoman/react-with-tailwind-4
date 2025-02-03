f2c360ef5deaa4493d45453575ae5be73995442a
Anamol Soman <anamolsoman2@gmail.com>
2025-02-03 16:08:51 +0530

    Configure tailwind 4.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div>
+          <Link>Logo</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
